85c4df6fe47284a5d5fd661b07e9ef8ae689723f
Jingtao Xu <jingtaozf@gmail.com>
2018-12-27 18:02:47 +0800

    add support to tangle org file.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject literate-clojure "0.0.1"
+(defproject literate-clojure "0.1.0"
   :description "a literate programming tool to write clojure in org mode"
   :url "http://github.com/jingtaozf/literate-clojure"
   :license {:name "EPL-2.0"
