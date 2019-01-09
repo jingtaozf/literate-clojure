4eb77e6df51cc1fe286effac198f77f24c58acec
Jingtao Xu <jingtaozf@gmail.com>
2019-01-09 21:45:30 +0800

    change header argument name from tangle to load

--- demo/project.clj
+++ demo/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [literate-clojure "0.0.1"]]
+                 [literate-clojure "0.1.1"]]
   :main ^:skip-aot demo.core
   :target-path "target/%s"
   :repl-options {:init-ns demo.core}
