9b8f50299651829a9bc48dd9408afc5aa2fb4a76
Jingtao Xu <jingtaozf@gmail.com>
2019-08-03 00:05:18 +0800

    fix linguist-language

--- demo/src/demo/literate_core.clj
+++ demo/src/demo/literate_core.clj
@@ -1,4 +1,4 @@
-# -*- encoding:utf-8 Mode: ORG;  -*- --- Enter into org syntax
+# -*- encoding:utf-8 Mode: ORG; eval: (poly-org-mode 1); -*- --- Enter into org syntax
 #+Startup: noindent
 * this is an example
 #+BEGIN_SRC clojure
