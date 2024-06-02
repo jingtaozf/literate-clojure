4f8ef89024d532119d4449d105b39254d0b1b877
Jingtao Xu <jingtaozf@gmail.com>
2024-06-02 08:42:10 +0800

    Upgrade library version to 0.3.1

--- demo/project.clj
+++ demo/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [literate-clojure "0.3.0"]
+                 [literate-clojure "0.3.1"]
                  [dorothy "0.0.6"]
                  [org.clojure/data.priority-map "1.0.0"]
                  ]
--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject literate-clojure "0.3.0"
+(defproject literate-clojure "0.3.1"
   :description "a literate programming tool to write clojure in org mode"
   :url "http://github.com/jingtaozf/literate-clojure"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.reader "1.3.2"]]
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org/repo"}]]
   :repositories [["snapshots" {:url "https://repo.clojars.org" :creds :gpg}]
                  ["releases" {:url "https://repo.clojars.org" :creds :gpg}]
                  ["alternate" {:url "https://repo.clojars.org" :creds :gpg}]]
